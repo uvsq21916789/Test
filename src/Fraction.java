d86d6f0289756b71f196335c46022ea37d437e3c
Fernando Marreros <fernandomk2017@gmail.com>
2020-10-20 01:38:37 +0100

    new commit 9

--- src/Fraction.java
+++ src/Fraction.java
@@ -5,15 +5,17 @@ public class Fraction {
 	/* private String firstName;
 	private String lastName; */
 	
-	/* public Fraction(String firstName, String lastName) {
+	public Fraction() {
+	}
+	/* 	String firstName, String lastName
 		this.firstName = firstName;
 		this.lastName = lastName;	
-	}
+	
 	
 @Override
 	public String toString() {
 		return firstName + " " + lastName;
-		} */
+		} */ 
 	
 public static void main(String[] args) {
 
---
+++ src/Fraction2.java
@@ -0,0 +1,5 @@
+public class Fraction2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
